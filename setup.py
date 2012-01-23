146e253997a67ce0a85a9946003540d951992f3e
Jan Schrewe <jan@schafproductions.com>
2012-01-23 21:30:09 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='mongoadmin',
-    version='0.1.1',
+    version='0.1.2',
     description="A replacement for django's admin that works with mongodb.",
     author='Jan Schrewe',
     author_email='jan@schafproductions.com',
