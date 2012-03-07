457216e2b4074288df5bcaa2c9870ff7fa92d6ef
Jan Schrewe <jan@schafproductions.com>
2012-03-07 04:17:48 +0100

    Models could get registered before django's admin site got replaced

--- mongoadmin/__init__.py
+++ mongoadmin/__init__.py
@@ -6,4 +6,9 @@ from django.conf import settings
 
 if getattr(settings, 'MONGOADMIN_OVERRIDE_ADMIN', False):
     import django.contrib.admin
+    # copy already registered model admins
+    # without that the already registered models
+    # don't show up in the new admin
+    site._registry = django.contrib.admin.site._registry
+    
     django.contrib.admin.site = site 
