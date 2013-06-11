ee2d577526768a97c4284797524dc2a1bf54f818
Jan Schrewe <jan@schafproductions.com>
2013-06-11 17:22:44 +0200

    Using try accept to import LOOKUP_SEP and QUERY_TERMS for backwards compatibility

--- mongoadmin/options.py
+++ mongoadmin/options.py
@@ -11,8 +11,11 @@ from django.core.paginator import Paginator
 from django.db import models, transaction, router
 from django.db.models.related import RelatedObject
 from django.db.models.fields import BLANK_CHOICE_DASH, FieldDoesNotExist
-from django.db.models.constants import LOOKUP_SEP
-from django.db.models.sql.constants import QUERY_TERMS
+try:
+    from django.db.models.sql.constants import LOOKUP_SEP, QUERY_TERMS
+except ImportError:
+    from django.db.models.constants import LOOKUP_SEP
+    from django.db.models.sql.constants import QUERY_TERMS
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.utils.decorators import method_decorator
