65eb7844db7906b953d9639701d726b8f6f45788
Jan Schrewe <jan@schafproductions.com>
2013-10-02 14:55:06 +0200

    Fix setup.py to close #23

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='mongoadmin',
     author='Jan Schrewe',
     author_email='jan@schafproductions.com',
     url='http://www.schafproductions.com/projects/mongo-admin/',
-    packages=['mongoadmin', 'mongoadmin.templatetags',],
+    packages=['mongoadmin', 'mongoadmin.templatetags', 'mongoadmin.contenttypes',],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
